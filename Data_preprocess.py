c43ec69a9180ceca4eb3711ba9f4184d7bfe1a48
Personal laptop <ferwanguer@me.com>
2022-09-09 10:02:51 +0200

    uniformation of the data

--- Data_preprocess.py
+++ Data_preprocess.py
@@ -1,6 +1,8 @@
 import os
 import re
-
+from scipy.interpolate import Rbf
+import numpy as np
+from random import random
 class FoilData:
     """Data extraction class"""
 
@@ -39,11 +41,48 @@ class FoilData:
             self.x_coordinates.append(x_coordinate)
             self.y_coordinates.append(y_coordinate)
 
+    def point_uniformation(self, desired_points = 66):
+        for i in range(len(self.x_coordinates)):
+            while len(self.x_coordinates[i]) > desired_points: #The +1 and -1 is to avoid eliminating the first or the last point (kutta point)
+                n = len(self.x_coordinates[i]) - 1
+                victim = int(random()*n)+1
+                self.x_coordinates[i].pop(victim)
+                self.y_coordinates[i].pop(victim)
+            while len(self.x_coordinates[i]) < desired_points:
+                n = len(self.x_coordinates[i])-1
+                new_father = int(random()*n)
+                new_x = 0.5*(self.x_coordinates[i][new_father] + self.x_coordinates[i][new_father+1])
+                new_y = 0.5 * (self.y_coordinates[i][new_father] + self.y_coordinates[i][new_father+1])
+                self.x_coordinates[i].insert(new_father+1, new_x)
+                self.y_coordinates[i].insert(new_father + 1, new_y)
+
+
 
 Data = FoilData('Airfoils')
+Data.point_uniformation()
 
+
+points =np.random.choice(len(Data.x_coordinates[0])-1,40,replace=False )
+
+x_new =0.5*( np.array(Data.x_coordinates[0])[points] + np.array(Data.x_coordinates[0])[np.array(points)+1])
+y_new = 0.5*( np.array(Data.y_coordinates[0])[points] + np.array(Data.y_coordinates[0])[np.array(points)+1])
+
+#
 from matplotlib import pyplot as plt
-
-plt.scatter(Data.x_coordinates[0], Data.y_coordinates[0])
+for i in range(8):
+    # print(i)
+    plt.plot(Data.x_coordinates[i], Data.y_coordinates[i])
+    # plt.scatter(x_new,y_new)
 plt.show()
+#
+# #VAMOS A INTENTAR UNA MOVIDA DE INTERPOLACION QUE PUEDE SALIR MAL.
+#
+# n = 1
 
+# rbf = Rbf(n*np.array(Data.x_coordinates[2]), n*np.array(Data.y_coordinates[2]), epsilon=4)
+#
+# def interpol(x,y):
+#     return (rbf(x)- y)**2
+#
+# print(np.sqrt(interpol(np.array([1]),np.array([0.2]))))
+# print("...")
--- small_test.py
+++ small_test.py
@@ -1,7 +1,3 @@
-import tensorflow as tf
-import numpy as np
-y_true = np.random.randint(0, 2, size=(2, 3))
-y_pred = np.random.random(size=(2, 3))
-print(y_true.shape,y_pred.shape)
-loss = tf.keras.losses.mean_squared_error(y_true, y_pred)
-print(loss)
+from random import random
+
+print(int(random()*3))
